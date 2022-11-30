41197c3f5f6f8300c2dc1d567c5ff75fea4fdec4
william0863 <granger.w35@gmail.com>
2022-11-30 14:25:04 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ”, followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+        /python/<text>: display “Python ”, followed by the value of the text
+        variable (replace underscore _ symbols with a space )
+            The default value of text is “is cool”
+    You must use the option strict_slashes=False in your route definition
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display text"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """display custom text given
+       first route statement ensures it works for:
+          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
+          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
