c6461d1ef37e0ed804776f9b6e707924882b080b
william0863 <granger.w35@gmail.com>
2022-11-30 11:08:05 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,7 +39,11 @@ class FileStorage:
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
-        from models.state import State
+        from models.state        'Review': Review
+                  }
+        try:
+            temp = {}
+     import State
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
@@ -47,11 +51,7 @@ class FileStorage:
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+                    with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
