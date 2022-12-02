e3b5a95f987fc73a35ff54365d9089be104d78c4
william0863 <granger.w35@gmail.com>
2022-12-02 11:06:11 +0100

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,82 +16,24 @@ Write a script that starts a Flask web application:
                 LI tag: description of one State: <state.id>:
                 <B><state.name></B>
 """
-from models import storage
-from models import *
 from flask import Flask, render_template
+from models import storage
+from os import getenv
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """display text"""
-    return "Hello HBNB!"
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """access File/DB Storage for all State objects and render to HTML"""
 
-
-@app.route('/hbnb')
-def hbnb():
-    """display text"""
-    return "HBNB"
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """display custom text given"""
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route('/python')
-@app.route('/python/<text>')
-def python_text(text="is cool"):
-    """display custom text given
-       first route statement ensures it works for:
-          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
-          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
-    """
-    return "Python {}".format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def text_if_int(n):
-    """display text only if int given"""
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_if_int(n):
-    """display html page only if int given
-       place given int into html template
-    """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def html_odd_or_even(n):
-    """display html page only if int given
-       place given int into html template
-       substitute text to display if int is odd or even
-    """
-    odd_or_even = "even" if (n % 2 == 0) else "odd"
-    return render_template('6-number_odd_or_even.html',
-                           n=n, odd_or_even=odd_or_even)
+    return render_template('7-states_list.html',
+                           states=[st for st in storage.all('State').values()])
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """after each request remove current SQLAlchemy session"""
+def teardown_db(exception):
+    """Closes the database again at the end of the request."""
     storage.close()
 
-
-@app.route('/states_list')
-def html_fetch_states():
-    """display html page
-       fetch sorted states to insert into html in UL tag
-    """
-    state_objs = [s for s in storage.all("State").values()]
-    return render_template('7-states_list.html',
-                           state_objs=state_objs)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
